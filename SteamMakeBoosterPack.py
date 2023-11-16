5df053cb75400b2f27552badb27c58752f1d6fb8
JimChen <jimchen@kronosresearch.com>
2023-11-16 01:56:41 +0000

    update login methods

--- SteamMakeBoosterPack.py
+++ SteamMakeBoosterPack.py
@@ -5,9 +5,14 @@ import steam
 import steam.webauth
 import steamfront
 import json
+import logging
 import re
 import time
 import datetime
+from requests import Session
+from base64 import b64encode
+from steam.core.crypto import rsa_publickey, pkcs1v15_encrypt
+from steam.utils.web import generate_session_id
 
 
 class get_info():
@@ -22,8 +27,8 @@ class get_info():
             self.inventory_id = config['ACCOUNT INFO']['inventory_id']
             self.game_id = json.loads(config['APP LIST']['game_id'])
             self.client = steamfront.Client()
-            print("Load config success!")
-            self.make_url = 'https://steamcommunity.com//tradingcards/ajaxcreatebooster'
+            logging.warning("Load config success!")
+            self.make_url = 'https://steamcommunity.com/tradingcards/ajaxcreatebooster'
             self.unpack_url = 'https://steamcommunity.com/id/{}/ajaxunpackbooster/'.format(self.inventory_id)
             self.headers = {
                 "Referer": "https://steamcommunity.com/tradingcards/boostercreator/",
@@ -32,20 +37,63 @@ class get_info():
         else:
             self.config_found = False
             self.game_id = []
-            print("Config file not detected, please create a new config file.")
+            logging.warning("Config file not detected, please create a new config file.")
 
     # Start make booster pack
     def login(self):
-        print("Getting login information...")
-        self.account = steam.webauth.WebAuth(self.username, self.password)
-        # print("Enter steam mobile authenticator:")
+        logging.warning("Getting login information...")
+        # self.account = steam.webauth.WebAuth(self.username, self.password)
+        # logging.warning("Enter steam mobile authenticator:")
         twofactor_code_inp = sys.argv[1]
         try:
-            self.account_session = self.account.login(twofactor_code=str(twofactor_code_inp))
-            print("Login success!")
+            logging.warning(f"Logging in as {self.username} using 2FA: {twofactor_code_inp}...")
+            # self.account_session = self.account.login(twofactor_code=str(twofactor_code_inp))
+            self.account_session = Session()
+            ua = "python-steam/{0} {1}".format("jimchen", self.account_session.headers['User-Agent'])
+            self.account_session.headers['User-Agent'] = ua
+            resp = self.account_session.post(
+                'https://steamcommunity.com/login/getrsakey/',
+                timeout=15,
+                data={
+                    'username': self.username,
+                    'donotcache': int(time.time() * 1000),
+                }
+            ).json()
+            key = rsa_publickey(
+                int(resp['publickey_mod'], 16),
+                int(resp['publickey_exp'], 16)
+            )
+            timestamp = resp['timestamp']
+            data = {
+                'username': self.username,
+                "password": b64encode(pkcs1v15_encrypt(key, self.password.encode('ascii'))),
+                "emailauth": "",
+                "emailsteamid": "",
+                "twofactorcode": twofactor_code_inp,
+                "captchagid": -1,
+                "captcha_text": "",
+                "loginfriendlyname": "python-steam webauth",
+                "rsatimestamp": timestamp,
+                "remember_login": 'true',
+                "donotcache": int(time.time() * 100000),
+            }
+            resp = self.account_session.post('https://steamcommunity.com/login/dologin/', data=data, timeout=15)
+            logging.warning(f"Login response: {resp.json()}")
+            domains = ['store.steampowered.com', 'help.steampowered.com', 'steamcommunity.com']
+            for cookie in list(self.account_session.cookies):
+                for domain in domains:
+                    self.account_session.cookies.set(cookie.name, cookie.value, domain=domain, secure=cookie.secure)
+
+            session_id = generate_session_id()
+            self.account_session.session_id = session_id
+            language = 'english'
+            for domain in domains:
+                self.account_session.cookies.set('Steam_Language', language, domain=domain)
+                self.account_session.cookies.set('birthtime', '-3333', domain=domain)
+                self.account_session.cookies.set('sessionid', session_id, domain=domain)
             return True
         except Exception as e:
-            print("Login failed, please check account info or authenticator code, error {}".format(e))
+            logging.exception("Login failed, please check account info or authenticator code, error {}".format(e))
             return False
             # Set main work
 
@@ -55,41 +103,42 @@ class get_info():
             return
         while True:
             params = {
-                "sessionid" : self.account.session_id,
+                "sessionid" : self.account_session.session_id,
                 "appid": -1,
                 "series": 1,
-                "tradability_preference" : 1
+                "tradability_preference" : 2
             }
             response = self.account_session.post(self.make_url, headers=self.headers, data=params)
             try:
                 goo_amount = int(re.search(r'"goo_amount":"([^"]+)"', response.text).group(1))
             except:
                 goo_amount = -1
-            print("Steam gems stock right now : %d" % goo_amount)
+            logging.warning(f"Steam gems stock right now : {goo_amount}")
             if goo_amount <= 1000:
-                print("Steam gems stock low, please re-fill.")
-                print("Current time :", str(datetime.datetime.now())[:19])
+                logging.warning("Steam gems stock low, please re-fill.")
+                logging.warning("Current time : {str(datetime.datetime.now())[:19]}")
                 time.sleep(60 * 60 * 12)
             else:
                 for gi in self.game_id:
                     time.sleep(0.1)
                     params = {
-                        "sessionid" : self.account.session_id,
+                        "sessionid" : self.account_session.session_id,
                         "appid": gi,
                         "series": 1,
-                        "tradability_preference" : 1
+                        "tradability_preference" : 2
                     }
                     response = self.account_session.post(self.make_url, headers=self.headers, data=params)
                     if response.text[2:18] != 'purchase_eresult':
-                        print("Puchase booster pack SUCCESS on game : %s-%s" % (gi, self.client.getApp(appid=gi).name))
+                        logging.warning("Puchase booster pack SUCCESS on game : %s-%s" % (gi, self.client.getApp(appid=gi).name))
                         search_result = re.search(r'"communityitemid":"([^"]+)"', response.text)
                         if search_result is None:
+                            logging.error(f"Can't find communityitemid on game : {gi}-{self.client.getApp(appid=gi).name}")
                             continue
                         item_id = search_result.group(1)
                         params = {
                             "appid": gi,
                             "communityitemid": item_id,
-                            "sessionid" : self.account.session_id
+                            "sessionid" : self.account_session.session_id
                         }
                         self.account_session.post(self.unpack_url, headers=self.headers, data=params)
                     else:
@@ -97,8 +146,8 @@ class get_info():
                             game_name = self.client.getApp(appid=gi).name
                         except:
                             game_name = "Unknown game"
-                        print("Purchase booster pack FAILED on game : %s-%s" % (gi, game_name))
-                print("Current time : " + str(datetime.datetime.now())[:19])
+                        logging.warning("Purchase booster pack FAILED on game : %s-%s" % (gi, game_name))
+                logging.warning("Current time : " + str(datetime.datetime.now())[:19])
                 time.sleep(60 * 60 * 6 + 60 * 5)  # Retry after 4 hours and 5 minutes
 
 
